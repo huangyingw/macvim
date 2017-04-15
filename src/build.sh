6e41f3cc64abc06ee4370f231dc854031bc5a859
Ying Huang <yinghuang@itutorgroup.com>
2017-04-15 11:27:03 -0700

    n

--- src/build.sh
+++ src/build.sh
@@ -4,6 +4,7 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
 ./configure --with-features=huge --enable-multibyte --with-macarchs=x86_64 --enable-perlinterp --enable-rubyinterp --enable-tclinterp --with-tlib=ncurses --with-compiledby=Homebrew --with-local-dir=/usr/local --enable-cscope --enable-pythoninterp \
-    && make
+    && make \
+    && make install
 
 cd -
