8a68fbf4a18d8f90ec2857a16a47134c101e7159
Ying Huang <yinghuang@itutorgroup.com>
2017-04-15 16:37:53 -0700

    n

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ cd src/
 ./configure --with-features=huge --enable-multibyte --with-macarchs=x86_64 --enable-perlinterp --enable-rubyinterp --enable-tclinterp --with-tlib=ncurses --enable-cscope --enable-pythoninterp \
     && make \
     && cd - \
-    && ln -fs "$SCRIPTPATH"/src/MacVim/mvim /usr/local/bin/vim \
-    && ~/loadrc/gitrc/gci.sh
+    && ~/loadrc/gitrc/gci.sh \
+    && ln -fs "$SCRIPTPATH"/src/MacVim/mvim /usr/local/bin/vim
 
 cd -
