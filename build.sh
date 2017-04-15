038ae3f4adae753865cb36daa0695f39fefb3b87
Ying Huang <yinghuang@itutorgroup.com>
2017-04-15 15:43:02 -0700

    n

--- build.sh
+++ build.sh
@@ -3,9 +3,11 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
+cd src/
 ./configure --with-features=huge --enable-multibyte --with-macarchs=x86_64 --enable-perlinterp --enable-rubyinterp --enable-tclinterp --with-tlib=ncurses --enable-cscope --enable-pythoninterp \
     && make \
+    && cd - \
     && ~/loadrc/gitrc/gci.sh \
-    && ln -fs ./MacVim/mvim /usr/local/bin/vim
+    && ln -fs ./src/MacVim/mvim /usr/local/bin/vim
 
 cd -
