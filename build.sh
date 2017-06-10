c3442ab7f7503183b2c32d237323aa97d4cd45e5
Ying Huang <yhuang@imvu.com>
2017-06-10 15:44:01 -0700

    n

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ cd "$SCRIPTPATH"
 cd src/
 
 xcode-select --install \
-    && brew reinstall python --framework \
+    ; brew reinstall python --framework \
     && ./configure --with-features=huge --enable-multibyte --with-macarchs=x86_64 --enable-perlinterp --enable-rubyinterp --enable-tclinterp --with-tlib=ncurses --with-local-dir=/usr/local --enable-cscope --enable-pythoninterp \
     && make \
     && cd - \
