fe2395b46c893a4ef61a4991926ed20e95afd978
Ying Huang <yhuang@imvu.com>
2017-07-14 11:12:52 -0700

    n

--- build.sh
+++ build.sh
@@ -9,6 +9,6 @@ xcode-select --install \
     ; brew reinstall python --framework \
     && ./configure --with-features=huge --enable-multibyte --with-macarchs=x86_64 --enable-perlinterp --enable-rubyinterp --enable-tclinterp --with-tlib=ncurses --with-local-dir=/usr/local --enable-cscope --enable-pythoninterp \
     && make \
-    && make install \
     && cd - \
-    && ~/loadrc/gitrc/gci.sh
+    && ~/loadrc/gitrc/gci.sh \
+    && cp -fv ./src/MacVim/build/Release/MacVim.app/Contents/MacOS/Vim /usr/local/bin/vim
