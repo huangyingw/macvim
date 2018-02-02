9345fe0d4f2cc173c36e8e90f931dff78021c543
Ying Huang <yhuang@imvu.com>
2018-02-01 18:49:15 -0800

    n

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ cd src/
 
 xcode-select --install \
     ; brew reinstall python --framework \
-    && ./configure --with-features=huge --enable-multibyte --with-macarchs=x86_64 --enable-perlinterp --enable-rubyinterp --enable-tclinterp --with-tlib=ncurses --with-local-dir=/usr/local --enable-cscope --enable-pythoninterp --enable-python3interp \
+    && ./configure --with-features=huge --enable-multibyte --with-macarchs=x86_64 --enable-perlinterp --enable-rubyinterp --enable-tclinterp --with-tlib=ncurses --with-local-dir=/usr/local --enable-cscope --enable-pythoninterp --with-python-config-dir=~/anaconda3/bin/python3-config \
     && make \
     && cd - \
     && ~/loadrc/gitrc/gci.sh \
